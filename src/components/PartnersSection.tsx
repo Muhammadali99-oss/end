ddccb23748b18385101485729ddffa6322f48d4a
Developer <developer@example.com>
2025-09-24 00:44:43 +0500

    Revert to YPC- base path and update all image paths accordingly

--- src/components/PartnersSection.tsx
+++ src/components/PartnersSection.tsx
@@ -2,27 +2,27 @@ const PartnersSection = () => {
   const partners = [
     {
       name: "Youth Affairs Agency",
-          logo: "/partners/youthaffairs.png",
+          logo: "/YPC-/partners/youthaffairs.png",
       url: "https://gov.uz/yoshlar"
     },
     {
       name: "Presidential Schools",
-      logo: "/partners/Presidentialschools.png",
+      logo: "/YPC-/partners/Presidentialschools.png",
       url: "https://presidentialschools.uz/"
     },
     {
       name: "Garnet Hotel",
-      logo: "/partners/Garnet.png",
+      logo: "/YPC-/partners/Garnet.png",
       url: "https://garnethotel.uz/"
     },
     {
       name: "Asaxiy Books",
-      logo: "/partners/Asaxiy.png",
+      logo: "/YPC-/partners/Asaxiy.png",
       url: "https://asaxiy.uz/"
     },
     {
       name: "Domino's Pizza",
-      logo: "/partners/Domino's.png",
+      logo: "/YPC-/partners/Domino's.png",
       url: "https://www.dominos.com/"
     }
   ];
